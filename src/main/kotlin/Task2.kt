3b2b0312967e25015fcd722bf123ed482a6e3a65
opreina81 <119162097+opreina81@users.noreply.github.com>
2023-04-17 21:20:06 +0800

    Update Task2

--- src/main/kotlin/Task2.kt
+++ src/main/kotlin/Task2.kt
@@ -6,24 +6,24 @@ import kotlin.math.roundToInt
 
 fun main() {
 
-    typeCasting("Privet")
-    typeCasting(145)
-    typeCasting(145.0)
-    typeCasting(145.2817812)
-    typeCasting(of(2006, 12, 24))
-    typeCasting(null)
-    typeCasting(Handler::class)
+    println(typeCasting("Privet"))
+    println(typeCasting(145))
+    println(typeCasting(145.0))
+    println(typeCasting(145.2817812))
+    println(typeCasting(of(2006, 12, 24)))
+    println(typeCasting(null))
+    println(typeCasting(Handler::class))
 
 }
 
-fun typeCasting(x: Any?) {
+fun typeCasting(x: Any?) : Any {
 
     if (x is String) {
-        println("Я получил тип String = \"$x\", ее длина равна ${x.length} символ")
+        return "Я получил тип String = \"$x\", ее длина равна ${x.length} символ"
     } else if (x is Int) {
-        println("Я получил тип Int = $x, его квадрат равен ${x * x}")
+        return "Я получил тип Int = $x, его квадрат равен ${x * x}"
     } else if (x is Double) {
-        println("Я получил Double = $x, это число округляется до ${(x * 100.0).roundToInt() / 100.0}")
+        return "Я получил Double = $x, это число округляется до ${(x * 100.0).roundToInt() / 100.0}"
     } else if (x is LocalDate) {
 
         val endDate = now()
@@ -32,16 +32,16 @@ fun typeCasting(x: Any?) {
         val tinkoffData = ChronoUnit.YEARS.between(tinkoffOfBirth, endDate).toInt()
 
         if (tinkoffData > xData) {
-            println("Я получил LocalDate $x, эта дата меньше, чем дата основания Tinkoff")
+            return "Я получил LocalDate $x, эта дата меньше, чем дата основания Tinkoff"
         } else if (tinkoffData < xData) {
-            println("Я получил LocalDate $x, эта дата больше, чем дата основания Tinkoff")
+            return "Я получил LocalDate $x, эта дата больше, чем дата основания Tinkoff"
         } else {
-            println("Я получил LocalDate $x. Вы одного возраста с Tinkoff!") //Alexey Belolipetskiy аппрувнул такое условие, согласовала с ним
+            return "Я получил LocalDate $x. Вы одного возраста с Tinkoff!" //Alexey Belolipetskiy аппрувнул такое условие, согласовала с ним
         }
 
     } else if (x == null) {
-        println("Oбъект равен null")
+        return "Oбъект равен null"
     } else {
-        println("Мне этот тип неизвестен(")
+        return "Мне этот тип неизвестен("
     }
 }
