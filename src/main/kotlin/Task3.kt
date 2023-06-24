226042f61669821fb30829300f64e87a41731d46
opreina81 <119162097+opreina81@users.noreply.github.com>
2023-06-24 20:26:17 +0800

    Update Task3.kt

--- src/main/kotlin/Task3.kt
+++ src/main/kotlin/Task3.kt
@@ -3,26 +3,24 @@ import kotlin.math.roundToInt
 fun main() {
     try {
         val numbers: List<Double?> = listOf(13.31, 3.98, 12.0, 2.99, 9.0)
-        print(sumOfCollection(numbers))
+        print(numbersRoundToInt(sumOfCollection(numbers)))
     } catch (e: Exception) {
         println(e.message)
     }
 }
 
 fun sumOfCollection(numbers: List<Double?>): Double {
-    var sum = 0.0
-    val parityCheck = numbers.filter { it!!.toInt() % 2 == 0 }.map { it!! * it!! }
-    val checkingForOdd = numbers.filter { it!!.toInt() % 2 != 0 }.map { it!! / 2 }
-    val newList = parityCheck.plus(checkingForOdd)
-    newList
+
+    return numbers
         .asSequence()
-        .filter { it!! < 25 }
         .filterNotNull()
+        .map { if (it.toInt() % 2 == 0) it * it else it / 2 }
+        .filter { it <= 25.0 }
         .sortedDescending()
         .take(10)
-        .toList()
-        .forEach { sum += it }
+        .sum()
+}
 
-    return (sum * 100.0).roundToInt() / 100.0
-
+fun numbersRoundToInt(numbers: Double): Double {
+    return (numbers * 100.0).roundToInt() / 100.0
 }
