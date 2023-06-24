44d141d4c2c1a88003dcccbcdae151b6c160e0d1
opreina81 <119162097+opreina81@users.noreply.github.com>
2023-06-24 20:25:06 +0800

    Update Task1.kt

--- src/main/kotlin/Task1.kt
+++ src/main/kotlin/Task1.kt
@@ -1,12 +1,8 @@
 fun main() {
-    val numbers: MutableList<Int> = mutableListOf()
-    print(numbers.square())
+    val numbers: MutableList<Int> = mutableListOf(1, 4, 9, 16, 25)
+    print(numbers.squareAllElementsMap())
 }
 
-fun <Int> MutableList<Int>.square(): MutableList<kotlin.Int> {
-    val numbers: MutableList<kotlin.Int> = mutableListOf(1, 4, 9, 16, 25)
-    numbers.indices.forEach { i ->
-        numbers[i] = numbers[i] * numbers[i]
-    }
-    return numbers
+fun MutableList<Int>.squareAllElementsMap(): MutableList<Int> {
+    return map { it * it }.toMutableList()
 }
