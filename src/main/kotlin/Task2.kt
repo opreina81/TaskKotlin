76c8e8bacdce0e5c3f79cc062fbb551c8482f728
opreina81 <119162097+opreina81@users.noreply.github.com>
2023-06-24 20:25:46 +0800

    Update Task2.kt

--- src/main/kotlin/Task2.kt
+++ src/main/kotlin/Task2.kt
@@ -10,38 +10,37 @@ fun main() {
     println(typeCasting(145))
     println(typeCasting(145.0))
     println(typeCasting(145.2817812))
-    println(typeCasting(of(2006, 12, 24)))
+
+    try {
+        println(typeCasting(of(2023, 5, 19)))
+    } catch (e: Exception) {
+        println(e.message)
+    }
+
     println(typeCasting(null))
     println(typeCasting(Handler::class))
-
 }
 
-fun typeCasting(x: Any?) : Any {
+fun typeCasting(x: Any?): Any {
+    when (x) {
+        is String -> return "Я получил тип String = \"$x\", ее длина равна ${x.length} символ"
+        is Int -> return "Я получил тип Int = $x, его квадрат равен ${x * x}"
+        is Double -> return "Я получил Double = $x, это число округляется до ${(x * 100.0).roundToInt() / 100.0}"
+        is LocalDate -> {
 
-    if (x is String) {
-        return "Я получил тип String = \"$x\", ее длина равна ${x.length} символ"
-    } else if (x is Int) {
-        return "Я получил тип Int = $x, его квадрат равен ${x * x}"
-    } else if (x is Double) {
-        return "Я получил Double = $x, это число округляется до ${(x * 100.0).roundToInt() / 100.0}"
-    } else if (x is LocalDate) {
-
-        val endDate = now()
-        val tinkoffOfBirth = of(2006, 12, 24)
-        val xData = ChronoUnit.YEARS.between(x, endDate).toInt()
-        val tinkoffData = ChronoUnit.YEARS.between(tinkoffOfBirth, endDate).toInt()
-
-        if (tinkoffData > xData) {
-            return "Я получил LocalDate $x, эта дата меньше, чем дата основания Tinkoff"
-        } else if (tinkoffData < xData) {
-            return "Я получил LocalDate $x, эта дата больше, чем дата основания Tinkoff"
-        } else {
-            return "Я получил LocalDate $x. Вы одного возраста с Tinkoff!" //Alexey Belolipetskiy аппрувнул такое условие, согласовала с ним
+            val endDate = now()
+            val tinkoffOfBirth = of(2006, 12, 24)
+            val xData = ChronoUnit.YEARS.between(x, endDate).toInt()
+            val tinkoffData = ChronoUnit.YEARS.between(tinkoffOfBirth, endDate).toInt()
+            if (x > endDate) throw Exception("Вы ввели заду из будущего! Оно еще не наступило :)")
+            return when {
+                (tinkoffData > xData) -> "Я получил LocalDate $x, эта дата больше, чем дата основания Tinkoff"
+                (tinkoffData < xData) -> "Я получил LocalDate $x, эта дата меньше, чем дата основания Tinkoff"
+                else -> "Я получил LocalDate $x. Вы одного возраста с Tinkoff!"
+            }
         }
 
-    } else if (x == null) {
-        return "Oбъект равен null"
-    } else {
-        return "Мне этот тип неизвестен("
+        null -> return "Oбъект равен null"
+        else -> return "Мне этот тип неизвестен("
     }
 }
